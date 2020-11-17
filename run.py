cf50943873ab1194bd9cbf21352e982b3adedac1
Amin <44217824+haeriamin@users.noreply.github.com>
2020-11-16 23:33:37 -0500

    Update run.py

--- run.py
+++ run.py
@@ -34,26 +34,26 @@ for sim_time in sim_times:
     # Initial solution
     if load:
         with open('last_solution.pkl', 'r') as f:
-            x0 = list(pickle.load(f))
-            print ' Last best solution:', x0[0], ',', x0[1], ',', x0[2], ',', x0[3], ',', x0[4]
+            X0 = list(pickle.load(f))
+            print ' Last best solution:', X0[0], ',', X0[1], ',', X0[2], ',', X0[3], ',', X0[4]
     else:
-        x0 = [0.7, 0.364, 0.012, 1.00, 20.0]
+        X0 = [0.7, 0.364, 0.012, 1.00, 20.0]
 
         # 5cm case -- 20 sec -- took 150 min -- ? fevals
-        # x0 = [6.60321784e-01, 3.98142579e-01, 1.16217771e-02, 2.20756469e-01, 1.78680532e+01] # 8.9% in 20
+        # X0 = [6.60321784e-01, 3.98142579e-01, 1.16217771e-02, 2.20756469e-01, 1.78680532e+01] # 8.9% in 20
 
         # 5cm case -- 5, 10, 20 sec -- took 30 min
-        # x0 = [6.83741285e-01, 3.35455728e-01, 1.10511245e-02, 2.11047748e-01, 1.98022255e+01]  # 8.2% (34 sec runtime and 1.2% mpe)
+        # X0 = [6.83741285e-01, 3.35455728e-01, 1.10511245e-02, 2.11047748e-01, 1.98022255e+01]  # 8.2% (34 sec runtime and 1.2% mpe)
 
         # 2cm case -- starting with 5cm best solution -- took 10 min
-        # x0 = [7.04127367e-01, 3.35380587e-01, 1.09857695e-02, 3.47156506e-01, 1.98045868e+01]  # 6.2% (24 sec runtime and ?% mpe)
+        # X0 = [7.04127367e-01, 3.35380587e-01, 1.09857695e-02, 3.47156506e-01, 1.98045868e+01]  # 6.2% (24 sec runtime and ?% mpe)
 
         # Both 2cm and 5cm cases -- 5, 10, 20 sec -- took 40 min
-        # x0 = [7.11653299e-01, 3.97859022e-01, 1.19631175e-02, 2.47373440e-01, 1.97698647e+01]  # 18.6% 
+        # X0 = [7.11653299e-01, 3.97859022e-01, 1.19631175e-02, 2.47373440e-01, 1.97698647e+01]  # 18.6% 
 
     # Optimizer
     vrtx = Excavation(sim_time, depths)
-    res = constrNM(vrtx.obj_func, x0, LB, UB, maxfun=200, full_output=True, disp=True, save=save)  # maxiter=200 
+    res = constrNM(vrtx.obj_func, X0, LB, UB, maxfun=200, full_output=True, disp=True, save=save)  # maxiter=200 
     del vrtx
 
     # Results
